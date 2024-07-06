2ff6b986dac9b6f4fbde18523dd1e6b3a2698455
bhaskarkrp <bhaskarkumar0022@gmail.com>
2024-07-06 20:11:12 +0530

    Favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata = {
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/assets/images/logo.svg" />
+      </head>
       <body>
         <Provider>
           <div className="main" key="main">
